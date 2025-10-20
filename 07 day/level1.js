168be24b741297c3f3a30105620bc4dac0c7dca9
SHAH HASAN <shahhasan@SHAHs-MacBook-Air.local>
2025-10-20 17:38:48 +0530

    removed some line

--- 07 day/level1.js
+++ 07 day/level1.js
@@ -107,27 +107,3 @@ console.log(weight());
 
 //QUESTION NO 12
 
-const rectangle = {
-  length: 20,
-  width: 20
-}
-console.log(rectangle) // 
-const person = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
-  age: 250,
-  country: 'Finland',
-  city: 'Helsinki',
-  skills: [
-    'HTML',
-    'CSS',
-    'JavaScript',
-    'React',
-    'Node',
-    'MongoDB',
-    'Python',
-    'D3.js'
-  ],
-  isMarried: true
-}
-console.log(person)
