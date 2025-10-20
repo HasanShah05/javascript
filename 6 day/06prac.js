aa084e2263bf0d0ed9851fbe589490f28a0ea8fd
SHAH HASAN <shahhasan@SHAHs-MacBook-Air.local>
2025-10-20 19:37:07 +0530

    first commit

---
+++ 6 day/06prac.js
@@ -0,0 +1,17 @@
+function square() {
+    let num = 2;
+    let sum = num * num ;
+    console.log(sum);
+    
+}
+square();
+
+let name = prompt("Enter your name")
+if (name == "hasan") {
+    console.log("hello buddy");
+    
+}
+else{
+    console.log("ohh nooo");
+    
+}
