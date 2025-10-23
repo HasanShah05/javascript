dca328fa6bd196d644a45bdb5335fe8b5c32488a
ShahHasan05 <shahhasanhanif@gmail.com>
2025-10-23 16:07:37 +0530

    day 9 learning

--- 09 day/hos.js
+++ 09 day/hos.js
@@ -5,4 +5,58 @@ const callback = (n) => {
 function cube(callback,n) {
     return callback(n) * n
 }
-console.log(cube(callback,3));
+// console.log(cube(callback,3));
+
+const highOrder = n => {
+    const doSomething = m => {
+    const doWhatever = t => {
+            return 2 * n + 3 * m + t
+        }
+        return doWhatever
+    }
+    return doSomething
+}
+// console.log(highOrder(2)(3)(10));
+
+
+//HIGH ORDER FUNCTION SELF UNDERSTANDING 
+function add(a,b, cb) {
+    let result = a + b
+    cb(result)
+}
+
+function showResult(result) {
+    // console.log(result);
+    
+}
+
+add(5,3,showResult)
+
+const map = new Map()
+
+map.set('IN',"India")
+map.set('FR',"France")
+map.set('USA',"United States Of America")
+
+for (const [key, value] of map) {
+    // console.log(key, ':-', value);
+    
+}
+
+const myObject = {
+    'Game 1': 'Valorant',
+    'Game 2': 'GTAV'
+}
+
+for (const key in myObject) {
+    // console.log(`this ia ${key} :- ${myObject[key]}`);
+    
+}
+
+const programming = ["js", 'rb', 'py', 'java', 'rb', 'cpp']
+
+for (const key in programming) {
+//   console.log(programming[key]);
+    
+}
+
