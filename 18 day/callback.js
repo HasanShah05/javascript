8211b23f4a70be6c00dd933cb11e87e2864ca16d
ShahHasan05 <shahhasanhanif@gmail.com>
2025-11-06 17:47:50 +0530

    in proccess

---
+++ 18 day/callback.js
@@ -0,0 +1,32 @@
+const doSomething = callback => {
+    setTimeout(() => {
+        const skills = ['Html','CSS','Javascript']
+        callback(false,skills)
+    },2000)
+}
+
+const callback = (err, result) => {
+    if (err) {
+        return console.log(err);
+    }
+    return console.log(result);
+}
+
+doSomething(callback)
+
+
+const doPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const skills = ['Html','Css','Javascript']
+        if (skills.length < 0) {
+            resolve(skills)
+        } else {
+            reject('something went wrong')
+        }
+    },2000)
+})
+
+doPromise
+.then(error => console.log(error))
+.catch(result => console.log(result))
+
