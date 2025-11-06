200d38dd204e28cf2fe421e3f636c2f3e0336f21
ShahHasan05 <shahhasanhanif@gmail.com>
2025-11-06 16:01:56 +0530

    completed

---
+++ 09 day/level2.js
@@ -0,0 +1,14 @@
+const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand']
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const products = [
+  { product: 'banana', price: 3 },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: 8 },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+]
+
+let totalPrice = products.filter(p => typeof p.price != 'string' ).reduce((acc, curr) => acc + curr.price,0)
+console.log(totalPrice);
